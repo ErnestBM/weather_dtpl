57ef92ea7ee5903294a94e55a0706d72ad0426c2
ernestbenedictus <m010d4ky1623@bangkit.academy>
2024-12-20 02:36:31 +0700

    deploy rabbit

--- rabbitmq/app/flatten.py
+++ rabbitmq/app/flatten.py
@@ -1,12 +1,15 @@
 def flatten_json(data: dict, parent_key='', sep='_') -> dict:
-    """
-    Flattens a nested JSON object.
-    """
     items = []
     for key, value in data.items():
         new_key = f"{parent_key}{sep}{key}" if parent_key else key
         if isinstance(value, dict):
             items.extend(flatten_json(value, new_key, sep=sep).items())
+        elif isinstance(value, list):
+            for i, item in enumerate(value):
+                if isinstance(item, dict):
+                    items.extend(flatten_json(item, f"{new_key}{sep}{i}", sep=sep).items())
+                else:
+                    items.append((f"{new_key}{sep}{i}", item))
         else:
             items.append((new_key, value))
     return dict(items)
